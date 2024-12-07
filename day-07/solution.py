01ec8467a7fc6375f99b4ee93ca393b91d38344c
Christian Drumm <christian.drumm@hey.com>
2024-12-07 08:25:23 +0100

    Solution for day 7

--- day-07/solution.py
+++ day-07/solution.py
@@ -15,14 +15,43 @@ def read_input_file(input_file_path: str) -> list:
     return [line.strip() for line in lines]
 
 
-def solve_part_1(data: list[str]) -> int:
-    return 0
+def is_possible_equation(equation: tuple[int, list[int]], concat_allowed: bool) -> bool:
+    r, ns = equation
+    results = [ns[0]]
+    for n in ns[1:]:
+        if concat_allowed:
+            results = (
+                [n * r for r in results]
+                + [n + r for r in results]
+                + [int(str(r) + str(n)) for r in results]
+                + [int(str(n) + str(r)) for r in results]
+            )
+        else:
+            results = [n * r for r in results] + [n + r for r in results]
+    return r in results
+
+
+def solve(data: list[str], concat_allowed: bool = False) -> int:
+    equations = [
+        (int(line.split(": ")[0]), list(map(int, line.split(": ")[1].split(" "))))
+        for line in data
+    ]
+    possible_eqs = list(
+        filter(lambda e: is_possible_equation(e, concat_allowed), equations)
+    )
+    return sum([r for r, ns in possible_eqs])
 
 
 @app.command()
 def part_1(input_file: str = "input.txt"):
     data = read_input_file(input_file)
-    print(f"The result is....")
+    print(f"The calibration result is {solve(data)}")
+
+
+@app.command()
+def part_2(input_file: str = "input.txt"):
+    data = read_input_file(input_file)
+    print(f"The calibration result is {solve(data, True)}")
 
 
 if __name__ == "__main__":
--- day-07/solution_test.py
+++ day-07/solution_test.py
@@ -3,18 +3,32 @@ from unittest import TestCase
 
 import solution
 
-test_string = """aaaa
-bbbb
-cccc
-dddd"""
+test_string = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
 
 
 class Testing(TestCase):
     def test_part_1(self):
-        r = solution.solve_part_1(test_string.split("\n"))
+        r = solution.solve(test_string.split("\n"))
 
         self.assertEqual(
-            1,
+            3749,
+            r,
+            "The result shoud be correct.",
+        )
+
+    def test_part_2(self):
+        r = solution.solve(test_string.split("\n"), True)
+
+        self.assertEqual(
+            11387,
             r,
             "The result shoud be correct.",
         )
