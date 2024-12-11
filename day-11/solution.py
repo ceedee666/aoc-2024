74a62e1d754cef65d1cc7161961c6c3e13ee67d6
Christian Drumm <christian.drumm@hey.com>
2024-12-11 10:49:30 +0100

    also implemented a solution using a dict withouht recursion and cache

--- day-11/solution.py
+++ day-11/solution.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import typer
-from itertools import chain
 from functools import cache
+from collections import defaultdict
 
 app = typer.Typer()
 
@@ -28,22 +28,44 @@ def blink(stone: int, iterations: int) -> int:
     return blink(stone * 2024, iterations - 1)
 
 
-def solve(data: list[str], blink_count: int = 25) -> int:
-    stones = [int(n) for n in data[0].split()]
+def blink2(stones: dict[int, int]) -> dict[int, int]:
+    new_stones = defaultdict(int)
+    for s, c in stones.items():
+        if s == 0:
+            new_stones[1] += c
+        elif len(str(s)) % 2 == 0:
+            s_str = str(s)
+            idx = len(s_str) // 2
+            new_stones[int(s_str[idx:])] += c
+            new_stones[int(s_str[:idx])] += c
+        else:
+            new_stones[s * 2024] += c
+    return new_stones
 
-    return sum([blink(s, blink_count) for s in stones])
+
+def solve(data: list[str], blink_count: int = 25, recursive: bool = True) -> int:
+    if recursive:
+        stones = [int(n) for n in data[0].split()]
+        return sum([blink(s, blink_count) for s in stones])
+    else:
+        stones = {int(n): 1 for n in data[0].split()}
+        for _ in range(blink_count):
+            stones = blink2(stones)
+        return sum(stones.values())
 
 
 @app.command()
-def part_1(input_file: str = "input.txt"):
+def part_1(input_file: str = "input.txt", recursive: bool = True):
     data = read_input_file(input_file)
-    print(f"There are {solve(data)} stones after blinking 25 times")
+    print(
+        f"There are {solve(data=data, recursive=recursive)} stones after blinking 25 times"
+    )
 
 
 @app.command()
-def part_2(input_file: str = "input.txt"):
+def part_2(input_file: str = "input.txt", recursive: bool = True):
     data = read_input_file(input_file)
-    print(f"There are {solve(data, 75)} stones after blinking 75 times")
+    print(f"There are {solve(data, 75, recursive)} stones after blinking 75 times")
 
 
 if __name__ == "__main__":
--- day-11/solution_test.py
+++ day-11/solution_test.py
@@ -24,6 +24,23 @@ class Testing(TestCase):
             "The result shoud be correct.",
         )
 
+    def test_part_1_wo_recursion(self):
+        r = solution.solve(test_string.split("\n"), 6, False)
+
+        self.assertEqual(
+            22,
+            r,
+            "The result shoud be correct.",
+        )
+
+        r = solution.solve(data=test_string.split("\n"), recursive=False)
+
+        self.assertEqual(
+            55312,
+            r,
+            "The result shoud be correct.",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
